d247edd7e7e7e2aba585496e660f85df5b9b38bd
Parvathykv-ship-it <parvathyanilkv2308@gmail.com>
2026-06-13 15:12:26 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,35 @@
+from email.message import EmailMessage
+import smtplib
+import schedule
+import time
+import os
+
+EMAIL = os.environ["EMAIL"]
+APP_PASSWORD = os.environ["APP_PASSWORD"]
+
+def send_reminder():
+
+    with open("taskbotreminder/tasks.txt", "r") as f:
+        tasks = f.read()
+
+    msg = EmailMessage()
+    msg["Subject"] = "Today's Tasks"
+    msg["From"] = EMAIL
+    msg["To"] = EMAIL
+
+    msg.set_content(tasks)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(EMAIL, APP_PASSWORD)
+        smtp.send_message(msg)
+
+    print("Reminder email sent!")
+
+
+schedule.every().day.at("7:00").do(send_reminder)
+
+print("Bot running...")
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
